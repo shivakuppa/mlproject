e7100a44bccb37321583ebab9445dccf2a4825b4
Shiva Kuppa <kuppashiva@gmail.com>
2025-10-06 18:10:12 +0530

    feat: make project deployment ready

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,9 @@ FROM python:3.12-slim
 WORKDIR /app
 COPY . /app/
 
+EXPOSE 5003
+
 RUN pip install -r requirements.txt
+
+
 CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
 
 if __name__=="__main__":
-    app.run(host="127.0.0.1", port=5003)        
+    app.run(host="0.0.0.0", port=5003)        
