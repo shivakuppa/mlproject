cf731156611c9134ad59f2c0aae6c43d0e1ccb75
Shiva Kuppa <kuppashiva@gmail.com>
2025-10-06 18:49:40 +0530

    removed pkl files from ignore

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 
@@ -40,4 +41,5 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5003)        
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)       
